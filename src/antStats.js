ee4766b367c30421f1cb59ffa21c7868b98ee030
Ramon Toth <ramontoth@yahoo.ca>
2021-07-31 11:11:22 -0400

    Linted

--- src/antStats.js
+++ src/antStats.js
@@ -1,71 +1,57 @@
-import net from 'net'
-
-// const host = {
-//   name: 'Worker1',
-//   ip: '192.168.1.6',
-//   port: 4028 
-// }
+import net from 'net';
 
 // Host config object: { name, ip, port}
 
- export function antStats(host = {name: '', ip: '0.0.0.0', port: 4028}) {
-    const socket = net.connect({
-        host: host.ip,
-        port: host.port,
-      });
+export function antStats(host = { name: '', ip: '0.0.0.0', port: 4028 }) {
+  const socket = net.connect({
+    host: host.ip,
+    port: host.port
+  });
 
-    return new Promise((resolve, reject)=>{
-      socket.on('error', function (err) {
-        resolve({
-          worker: host.name, 
-          ip: host.ip,
-          online: false,
-          stats: null
-        })
-        // return reject(err);
+  return new Promise((resolve, reject) => {
+    socket.on('error', function (err) {
+      resolve({
+        worker: host.name,
+        ip: host.ip,
+        online: false,
+        stats: null
       });
+      // return reject(err);
+    });
 
-      socket.on('connect', () => {
-          socket.write('stats')
-        var buffer = '';
-        socket.on('data',  (data) => {
-          buffer += data.toString();
-        });
-        socket.on('end',  () => {
-          try {
-            const json = parseBuffer(buffer)            
-            resolve({
-              worker: host.name,
-              ip: host.ip,
-              online: true,
-              stats: json
-            });
-          }
-          catch (e) {
-              reject(e)
-          }
-        });
+    socket.on('connect', () => {
+      socket.write('stats');
+      var buffer = '';
+      socket.on('data', (data) => {
+        buffer += data.toString();
       });
-    })
-
-  }
+      socket.on('end', () => {
+        try {
+          const json = parseBuffer(buffer);
+          resolve({
+            worker: host.name,
+            ip: host.ip,
+            online: true,
+            stats: json
+          });
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+}
 
 function parseBuffer(buffer) {
-    const str = buffer
-            .replace(/\-nan/g, '0')
-            .replace(/[^\x00-\x7F]/g, '');
+  const str = buffer.replace(/\-nan/g, '0').replace(/[^\x00-\x7F]/g, '');
 
-    let array = str.split(',')
-    array = array.map(e => { 
-        return { [e.split('=')[0]]: e.split('=')[1]}
-    })
-    let json = {}
-    array.forEach(e => {
-        Object.assign(json, e)
-    })
-    return json
-
-  }
-
-// const stats = await antStats(host)
-// console.log(stats)
+  let array = str.split(',');
+  array = array.map((e) => {
+    return { [e.split('=')[0]]: e.split('=')[1] };
+  });
+  let json = {};
+  array.forEach((e) => {
+    Object.assign(json, e);
+  });
+  return json;
+}
