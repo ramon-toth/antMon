88ad8dc361e90418ed60b8973aa0f822ddcd9072
Ramon Toth <ramontoth@yahoo.ca>
2021-07-31 14:48:16 -0400

    format update

--- src/config.js
+++ src/config.js
@@ -2,41 +2,48 @@ export const SERVER = 'ws://76.66.102.73:3033';
 
 export const INTERVAL = 10;
 
-export const FARM = 'Office';
+export const FARM = { name: 'Office', id: 1 };
 
 export const WORKERS = [
   {
     name: 'Worker1',
+    workerID: 1,
     ip: '192.168.1.6',
     port: 4028
   },
   {
     name: 'Worker2',
+    workerID: 2,
     ip: '192.168.1.7',
     port: 4028
   },
   {
     name: 'Worker3',
+    workerID: 3,
     ip: '192.168.1.8',
     port: 4028
   },
   {
     name: 'Worker4',
+    workerID: 4,
     ip: '192.168.1.9',
     port: 4028
   },
   {
     name: 'Worker5',
+    workerID: 5,
     ip: '192.168.1.10',
     port: 4028
   },
   {
     name: 'Worker6',
+    workerID: 6,
     ip: '192.168.1.11',
     port: 4028
   },
   {
     name: 'Worker7 DOES NOT EXIST',
+    workerID: 7,
     ip: '192.168.1.18',
     port: 4028
   }
