dd3acfea05ec337adeecc1849516b28d4fe9ef7c
Ramon Toth <ramontoth@yahoo.ca>
2021-10-15 21:03:12 -0400

    Timestamps added

--- src/index.js
+++ src/index.js
@@ -8,17 +8,17 @@ dotenv.config();
 const socket = io(SERVER, { auth: { token: process.env.API_KEY } });
 
 socket.emit('farm', FARM);
-socket.on('connect', () => console.log('Connected to Server at', SERVER));
+socket.on('connect', () => console.log(timestamp(), 'Connected to Server at', SERVER));
 
 socket.on('command', (data) => {
-  console.log('Message from server:', data); // Not implemented
+  console.log(timestamp(), 'Message from server:', data); // Not implemented
 });
 
 socket.on('connect_error', (err) => {
-  console.log('Connection Error', err);
+  console.log(timestamp(), 'Connection Error');
 });
 
-socket.on('disconnect', () => console.log('Server disconnected'));
+socket.on('disconnect', () => console.log(timestamp(), 'Server disconnected'));
 
 function fetchAllStats() {
   const stats = WORKERS.map(async (w) => await antStats(w));
@@ -26,6 +26,10 @@ function fetchAllStats() {
   Promise.all(stats)
     .then((stats) => socket.emit('message', stats))
     .catch((e) => console.log(e));
+}
+
+export function timestamp() {
+  return `[${new Date().toLocaleString('en-US')}] -`;
 }
 
 setInterval(fetchAllStats, INTERVAL * 1000);
